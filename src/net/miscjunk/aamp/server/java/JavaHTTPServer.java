6b7ef329d692d80ed47bc3cfc69a1dbb24677541
Michael Limiero <mike5713@gmail.com>
2013-09-21 16:11:11 -0400

    Move stuff to common

--- src/net/miscjunk/aamp/server/java/JavaHTTPServer.java
+++ src/net/miscjunk/aamp/server/java/JavaHTTPServer.java
@@ -1,26 +1,33 @@
 package net.miscjunk.aamp.server.java;
 
-import net.miscjunk.aamp.common.AppListener;
-import net.miscjunk.aamp.common.Player;
+import net.miscjunk.aamp.common.*;
 
 import org.eclipse.jetty.server.Server;
 
+import com.google.gson.GsonBuilder;
+
 public class JavaHTTPServer extends AppListener {
 
-	public JavaHTTPServer(Player handler) {
-		super(handler);
-	}
+    public JavaHTTPServer(Player handler) {
+        super(handler);
+    }
 
-	@Override
-	public void start() {
-		Server server = new Server(13531);
-		server.setHandler(new HttpPlayerHandler(handler));
-		try {
-			server.start();
-			System.out.println("Listening on " + 13531);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void start() {
+        GsonBuilder gb = new GsonBuilder();
+        gb.registerTypeAdapter(Song.class, new SongSerializer());
+        gb.registerTypeAdapter(Playlist.class, new PlaylistDeserializer(this.handler));
+        gb.registerTypeAdapter(MusicProvider.class, new MusicProviderDeserializer());
+        gb.registerTypeAdapter(LocalFolderProvider.class, new LocalFolderProviderSerializer());
+        gb.registerTypeAdapter(LocalFolderProvider.class, new LocalFolderProviderDeserializer());
+        Server server = new Server(13531);
+        server.setHandler(new HttpPlayerHandler(handler, gb.create()));
+        try {
+            server.start();
+            System.out.println("Listening on " + 13531);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
