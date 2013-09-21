79ee507993af684f380560a6f1204b7f415342c7
Michael Limiero <mike5713@gmail.com>
2013-09-21 10:16:20 -0400

    GET /songs

--- src/net/miscjunk/aamp/server/java/HttpPlayerHandler.java
+++ src/net/miscjunk/aamp/server/java/HttpPlayerHandler.java
@@ -9,40 +9,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.miscjunk.aamp.common.Player;
+import net.miscjunk.aamp.common.Playlist;
+import net.miscjunk.aamp.common.Song;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class HttpPlayerHandler extends AbstractHandler {
-	private Player player;
-	
-	public HttpPlayerHandler(Player appHandler) {
-		this.player = appHandler;
-	}
-	
-	@Override
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    private Player player;
+    private Gson gson;
+
+    public HttpPlayerHandler(Player appHandler) {
+        this.player = appHandler;
+        GsonBuilder gb = new GsonBuilder();
+        gb.registerTypeAdapter(Song.class, new SongSerializer());
+        gson = gb.create();
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         System.out.println("Got target " + target);
-        if(target.startsWith("/control/") && "post".equalsIgnoreCase(request.getMethod()) ) {
-        	BufferedReader bodyReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
-        	String action = bodyReader.readLine();
-        	bodyReader.close();
-        	System.out.println("With action " + action);
-        	if("play".equals(action)) {
-        		player.play();
-        	}else if("pause".equals(action)) {
-        		player.pause();
-        	}else if(action.startsWith("volume=")) {
-        		player.setVolume(Double.parseDouble(action.replace("volume=", "")));
-        	}else if(action.startsWith("seek=")) {
-        		player.seek(Double.parseDouble(action.replace("seek=", "")));
-    		}
-        	response.getWriter().println("Running " + target);
+        if(target.startsWith("/control") && "post".equalsIgnoreCase(request.getMethod()) ) {
+            BufferedReader bodyReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String action = bodyReader.readLine();
+            bodyReader.close();
+            System.out.println("With action " + action);
+            if("play".equals(action)) {
+                player.play();
+            } else if("pause".equals(action)) {
+                player.pause();
+            } else if(action.startsWith("volume=")) {
+                player.setVolume(Double.parseDouble(action.replace("volume=", "")));
+            } else if(action.startsWith("seek=")) {
+                player.seek(Double.parseDouble(action.replace("seek=", "")));
+            }
+            response.getWriter().println("Running " + target);
+        } else if (target.startsWith("/songs") && "get".equalsIgnoreCase(request.getMethod())) {
+           Playlist allSongs = player.getAllSongs();
+           String json = gson.toJson(allSongs.getSongs());
+           response.getOutputStream().print(json);
         }
-        
-	}
 
-	
+    }
+
+
 }
